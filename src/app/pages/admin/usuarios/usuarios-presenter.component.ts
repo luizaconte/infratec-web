ea286f99fea287a6e36a39cb7d4cab77ce94af7a
luiza silva conte <silvaconte@hotmail.com>
2024-09-17 21:01:01 -0300

    cadastro de usuarios

--- src/app/pages/admin/usuarios/usuarios-presenter.component.ts
+++ src/app/pages/admin/usuarios/usuarios-presenter.component.ts
@@ -1,14 +1,14 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'sales-usuarios-presenter',
+  selector: 'infratec-usuarios-presenter',
   template: `
-    <sales-cadastro-base-layout #presenter [crumbs]="['Administração']" [(model)]="container.usuario" [cadastroBase]="container.cadastroBase"
+    <infratec-cadastro-base-layout #presenter [crumbs]="['Administração']" [(model)]="container.usuario" [cadastroBase]="container.cadastroBase"
                               [dadosBase]="container.dadosBase" [enableHandler]="container.enableHandler" [current]="container.cadastroBase.page.currentCrumb"
                               [beforeAction]="container.beforeAction" [autoSearch]="true" >
-      <sales-usuarios-container #container [listMode]="presenter.listMode" [enableHandler]="presenter.enableHandler" [beforeAction]="presenter.beforeAction">
-      </sales-usuarios-container>
-    </sales-cadastro-base-layout>`
+      <infratec-usuarios-container #container [listMode]="presenter.listMode" [enableHandler]="presenter.enableHandler" [beforeAction]="presenter.beforeAction">
+      </infratec-usuarios-container>
+    </infratec-cadastro-base-layout>`
 })
 export class UsuariosPresenterComponent {
 }
